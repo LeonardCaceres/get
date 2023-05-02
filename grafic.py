079ca2d0186164dee166fe12b123560c7ab2c5ac
Caceres Leonard <125807225+LeonardCaceres@users.noreply.github.com>
2023-05-02 20:01:56 +0300

    Update grafic.py

--- grafic.py
+++ grafic.py
@@ -10,7 +10,7 @@ data=numpy.loadtxt('data.txt', dtype=int) * settings[1]
 #массив времен, создаваемый черед количество измерений и известный шаг по времени
 data_time=numpy.array([i*settings[0] for i in range(data.size)])
 #параметры фигуры
-fig, ax=pyplot.subplots(figsize=(16, 10), dpi=500)
+fig, ax=pyplot.subplots()
 
 #минимальные и максимальные значения для осей
 ax.axis([data.min(), data_time.max()+1, data.min(), data.max()+0.2])
@@ -25,23 +25,21 @@ ax.yaxis.set_major_locator(ticker.MultipleLocator(0.5))
 ax.yaxis.set_minor_locator(ticker.MultipleLocator(0.1))
 
 #название графика с условием для переноса строки и центрированием
-ax.set_title("\n".join(wrap('процесс заряда и разряда конденсатора в RC цепи', 60)), loc = 'center')
+ax.set_title("\n".join(wrap('Изучение зарядки и разрядки конденсатора в RC цепи', 60)), loc = 'center')
 
 #сетка основная и второстепенная
-ax.grid(which='major', color = 'k')
+ax.grid(which='major', color = 'black')
 ax.minorticks_on()
 ax.grid(which='minor', color = 'gray', linestyle = ':')
 
 #подпись осей
-ax.set_ylabel("напряжение, В")
-ax.set_xlabel("время, с")
+ax.set_ylabel("U(напр.), В")
+ax.set_xlabel("t(вр), с")
 
 #линия с легендой
-ax.plot(data_time, data, c='black', linewidth=1, label = 'V(t)')
-ax.scatter(data_time[0:data.size:20], data[0:data.size:20], marker = 's', c = 'blue', s=10)
+ax.plot(data_time, data, color='black', linewidth=1, label = 'V(t)')
+ax.scatter(data_time[0:data.size:20], data[0:data.size:20], marker = 's', c = 'red', s=10)
 
-ax.legend(shadow = False, loc = 'right', fontsize = 30)
-
+ax.legend(shadow = False, loc = 'center right', fontsize = 20)
 #сохранение
-fig.savefig('graph.png')
-fig.savefig('graph.svg')
+pyplot.show()
